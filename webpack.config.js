a8af3ef1a7a2efb9c557c3e13328541969b4475f
Johan Daniel R Garcia <daniel.garcia@tambourine.com>
2023-02-11 16:50:56 -0500

    feat: init files of configuration project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/images/[hash][ext]',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+}
